bb05c4c0d31faa5c474d2187fab4ea831541e626
ryancosta-bit <ryan.costa@sinodalprado.com.br>
2025-11-11 16:30:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,72 +1,49 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-app.use(express.static(path.join(__dirname, "public")));
+const PORT = process.env.PORT || 3000;
 
-let jogadores = {};
-let votacao = null;
+app.use(express.static("public"));
+
+let jogadores = {}; // {socketId: {nick, escolha, cor}}
 
 io.on("connection", (socket) => {
   console.log("Novo jogador conectado:", socket.id);
 
-  // Registrar jogador
-  socket.on("registrar", (nick) => {
-    jogadores[socket.id] = {
-      nick,
-      escolha: null,
-      x: 0,
-      y: 0,
-      cor: "#" + Math.floor(Math.random() * 16777215).toString(16)
-    };
-
-    if (nick.toLowerCase() === "mestre") {
-      socket.emit("painelMestre");
-    }
-
+  socket.on("registrar", nick => {
+    const cor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    jogadores[socket.id] = {nick, escolha: "cima", cor};
     io.emit("jogadores", jogadores);
   });
 
-  // Atualizar posição/escolha
-  socket.on("atualizarEscolha", (escolha) => {
-    if (jogadores[socket.id]) {
+  socket.on("atualizarEscolha", escolha => {
+    if(jogadores[socket.id]){
       jogadores[socket.id].escolha = escolha;
       io.emit("jogadores", jogadores);
     }
   });
 
-  // Nova votação
-  socket.on("novaVotacao", (data) => {
-    votacao = {
-      opcoes: data.opcoes,
-      tempo: data.tempo
-    };
-    io.emit("iniciarVotacao", votacao);
-  });
-
-  // Limpar seleções (mestre)
   socket.on("limparSelecoes", () => {
-    for (let j in jogadores) {
-      if (jogadores[j].nick.toLowerCase() !== "mestre") {
-        jogadores[j].escolha = null;
-      }
+    for(const j of Object.values(jogadores)){
+      if(j.nick.toLowerCase()!=="mestre") j.escolha = null;
     }
     io.emit("jogadores", jogadores);
   });
 
-  // Desconexão
+  socket.on("novaVotacao", data => {
+    io.emit("iniciarVotacao", data);
+  });
+
+  socket.on("atualizarTexto", data => {
+    socket.broadcast.emit("atualizarTexto", data);
+  });
+
   socket.on("disconnect", () => {
     delete jogadores[socket.id];
     io.emit("jogadores", jogadores);
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log("Servidor rodando na porta", PORT);
-});
+http.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
